069e44835a683a8d997bd937872857c1df544b25
SuziZaitunNurkamal <szaitunnk@gmail.com>
2025-06-10 22:36:24 +0800

    - Splash screen sudah menggunakan Bahasa Indonesia - Splash screen mengikuti dark/light theme

--- app/src/main/java/com/project/finalmobile/zaitunes/ui/activity/SplashActivity.java
+++ app/src/main/java/com/project/finalmobile/zaitunes/ui/activity/SplashActivity.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatDelegate;
 import com.project.finalmobile.R;
+import com.project.finalmobile.zaitunes.util.ThemePreference;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Set theme sebelum super.onCreate
+        ThemePreference themePref = new ThemePreference(this);
+        if (themePref.isDarkMode()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
